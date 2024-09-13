82322396c8907aec3b35fe9fdd6e10cebb054bdf
MeetAntala45 <meetantala2005@gmail.com>
2024-09-13 18:41:47 +0530

    logo added at app starting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Invoice Generator',
-      home: LoginPage(),
+      home: SplashScreen(),
     ));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+  }
+}
